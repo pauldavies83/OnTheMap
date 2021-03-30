cd3d5dff99412bcb9d1884918f4b4ddf5d896ba8
pauldavies83 <pauldavies83@googlemail.com>
2021-03-30 14:14:45 +0100

    add new pin - view controller and navigation bar button to open it

---
+++ OnTheMap/Controller/AddPinViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AddPinViewController.swift
+//  OnTheMap
+//
+//  Created by Paul Davies on 30/03/2021.
+//
+
+import UIKit
+
+class AddPinViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func cancel(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
--- OnTheMap/Controller/PinsViewController.swift
+++ OnTheMap/Controller/PinsViewController.swift
@@ -17,7 +17,9 @@ class PinsViewController: UIViewController {
     var refreshDataCompleted: (() -> Void)? = nil
     
     let uiBusy = UIActivityIndicatorView(style: UIActivityIndicatorView.Style.medium)
+    var busyButton: UIBarButtonItem?
     var refreshButton: UIBarButtonItem?
+    var addButton: UIBarButtonItem?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,7 +29,10 @@ class PinsViewController: UIViewController {
 
         self.navigationItem.title = "On The Map"
         self.navigationItem.leftBarButtonItem = UIBarButtonItem(title: "LOGOUT", style: .plain, target: self, action: #selector(self.logout))
+        
+        busyButton = UIBarButtonItem(customView: uiBusy)
         refreshButton = UIBarButtonItem(image: UIImage(named: "icon_refresh"), style: .plain, target: self, action: #selector(self.refresh))
+        addButton = UIBarButtonItem(image: UIImage(named: "icon_addpin"), style: .plain, target: self, action: #selector(self.addPin))
 
         configureUI(isLoading: false)
     }
@@ -45,26 +50,28 @@ class PinsViewController: UIViewController {
     }
     
     @objc func refresh() {
-        let uiBusy = UIActivityIndicatorView(style: UIActivityIndicatorView.Style.medium)
-        uiBusy.hidesWhenStopped = true
-        uiBusy.startAnimating()
-        uiBusy.isUserInteractionEnabled = false
-        self.navigationItem.rightBarButtonItem = UIBarButtonItem(customView: uiBusy)
+        configureUI(isLoading: true)
 
         locationsDataSource.refreshData { (error) in
-            uiBusy.stopAnimating()
-            self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(named: "icon_refresh"), style: .plain, target: self, action: #selector(self.refresh))
+            self.configureUI(isLoading: false)
             self.refreshDataCompleted?()
         }
     }
+    
+    @objc func addPin() {
+        let next = self.storyboard?.instantiateViewController(identifier: "AddPinNavigationController") as! UINavigationController
+        next.modalPresentationStyle = .fullScreen
+        self.present(next, animated: true, completion: nil)
+    }
 
     func configureUI(isLoading: Bool) {
+        self.navigationItem.rightBarButtonItems = []
         if isLoading {
             uiBusy.startAnimating()
-            self.navigationItem.rightBarButtonItem = UIBarButtonItem(customView: uiBusy)
+            self.navigationItem.rightBarButtonItems = [addButton!, busyButton!]
         } else {
             uiBusy.stopAnimating()
-            self.navigationItem.rightBarButtonItem = refreshButton
+            self.navigationItem.rightBarButtonItems = [addButton!, refreshButton!]
         }
     }
 
