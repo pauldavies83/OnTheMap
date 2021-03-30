eaff0454406f582eec9d92897c5712934fc0142a
pauldavies83 <pauldavies83@googlemail.com>
2021-03-30 19:39:53 +0100

    decode location and present on map for verification

--- OnTheMap/Controller/AddPinViewController.swift
+++ OnTheMap/Controller/AddPinViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import MapKit
 
 class AddPinViewController: UIViewController {
 
@@ -14,6 +15,8 @@ class AddPinViewController: UIViewController {
     @IBOutlet weak var findLocationButton: UIButton!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
+    let geocoder = CLGeocoder()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         findLocationButton.layer.cornerRadius = 5
@@ -21,15 +24,47 @@ class AddPinViewController: UIViewController {
         configureUI(isLoading: false)
     }
     
+    fileprivate func showVerifyLocation(location: CLLocation, description: String?) {
+        let vc = self.storyboard?.instantiateViewController(withIdentifier: "VerifyLocationViewController") as! VerifyLocationViewController
+        vc.location = location
+        vc.locationText = description ?? locationText.text
+        vc.linkText = linkText.text
+        self.present(vc, animated: true, completion: nil)
+    }
+    
     @IBAction func findLocation(_ sender: Any) {
-        configureUI(isLoading: true)
-        // look up geoLocation
-        performSegue(withIdentifier: "verifyLocation", sender: sender)
-        configureUI(isLoading: false)
+        if linkText.text == nil || linkText.text?.count == 0 {
+            presentErrorAlert(title: "Link was empty", message: "Please enter a value for link")
+            return
+        }
+        
+        if let addressString = locationText.text {
+            configureUI(isLoading: true)
+
+            geocoder.geocodeAddressString(addressString) { (placemarks, error) in
+                self.configureUI(isLoading: false)
+                if error != nil {
+                    self.presentErrorAlert(title: "Location lookup failed", message: error?.localizedDescription)
+                    return
+                }
+                if let location = placemarks?.first?.location {
+                    self.showVerifyLocation(location: location, description: placemarks!.first!.name)
+                }
+            }
+        } else {
+            return
+        }
     }
     
     @IBAction func cancel(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    fileprivate func presentErrorAlert(title: String, message: String?) {
+        let alertVC = UIAlertController(title: title, message: message?.description ?? "", preferredStyle: .alert)
+        alertVC.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        alertVC.modalPresentationStyle = .overFullScreen
+        self.present(alertVC, animated: true, completion: nil)
     }
     
     func configureUI(isLoading: Bool) {
--- OnTheMap/Controller/VerifyLocationViewController.swift
+++ OnTheMap/Controller/VerifyLocationViewController.swift
@@ -6,18 +6,49 @@
 //
 
 import UIKit
+import MapKit
 
-class VerifyLocationViewController: UIViewController {
+class VerifyLocationViewController: UIViewController, MKMapViewDelegate {
 
     @IBOutlet weak var finishButton: UIButton!
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var location: CLLocation?
+    var locationText: String?
+    var linkText: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         finishButton.layer.cornerRadius = 5
+        
+        mapView.delegate = self
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = CLLocationCoordinate2D(latitude: location!.coordinate.latitude, longitude: location!.coordinate.longitude)
+        annotation.title = locationText!
+        mapView.addAnnotation(annotation)
+        
+        let region = MKCoordinateRegion(center: annotation.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
+        mapView.setRegion(region, animated: true)
     }
     
     @IBAction func finish(_ sender: Any) {
         // post to API
         dismiss(animated: true, completion: nil)
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let identifier = "annotation"
+        var view: MKMarkerAnnotationView
+        if let dequeuedView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKMarkerAnnotationView {
+            dequeuedView.annotation = annotation
+            view = dequeuedView
+        } else {
+            view = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            view.canShowCallout = true
+            view.calloutOffset = CGPoint(x: -5, y: 5)
+            view.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        }
+        return view
     }
 }
